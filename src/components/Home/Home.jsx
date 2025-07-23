907b81c2d982ad2d864803179cb0f0c8567f6955
Maryam-Yehia <11410120220333@stud.cu.edu.eg>
2025-07-23 23:58:37 +0300

    sec commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
+
+  useEffect(() => {
+        document.title = "home ";
+      }, []); 
+    
+
   return (
     <>
 
